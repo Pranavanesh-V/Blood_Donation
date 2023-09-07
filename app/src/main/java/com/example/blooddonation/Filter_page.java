6cb82030893cfca231442cc0e772858b60cfa1de
Pranavanesh-V <pranavanesh.vimalraj@gmail.com>
2023-09-08 00:13:32 +0530

    registration page

---
+++ app/src/main/java/com/example/blooddonation/Filter_page.java
@@ -0,0 +1,141 @@
+package com.example.blooddonation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.PopupMenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class Filter_page extends AppCompatActivity {
+
+    TextInputLayout Gender,Blood_group,Age;
+    String S_Gender,S_Blood_group,S_Age;
+    TextInputEditText E_Gender,E_Age,E_B_G;
+    Button Done;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_page);
+
+        Gender=findViewById(R.id.Gender);
+        E_Gender=findViewById(R.id.E_Gender);
+        //blood group
+        Blood_group=findViewById(R.id.Blood_group);
+        E_B_G=findViewById(R.id.E_bg);
+        //Age
+        Age=findViewById(R.id.Age);
+        E_Age=findViewById(R.id.E_age);
+        //Done button
+        Done=findViewById(R.id.Done);
+
+        E_B_G.setOnClickListener(view -> showPopupMenu());
+
+        E_Gender.setOnClickListener(view -> showPopupMenuForGender());
+
+        E_Age.setOnClickListener(view -> showPopupMenuForAge());
+
+        Done.setOnClickListener(view -> {
+            Intent intent=new Intent();
+            intent.putExtra("Age",S_Age);
+            intent.putExtra("Gender",S_Gender);
+            intent.putExtra("Blood_group",S_Blood_group);
+            setResult(RESULT_OK,intent);
+            finish();
+        });
+
+
+
+
+    }
+
+    //For blood group
+    private void showPopupMenu() {
+        PopupMenu popupMenu = new PopupMenu(this,Blood_group);
+        popupMenu.getMenu().add("None");
+        popupMenu.getMenu().add("O+");
+        popupMenu.getMenu().add("O-");
+        popupMenu.getMenu().add("A+");
+        popupMenu.getMenu().add("A-");
+        popupMenu.getMenu().add("B+");
+        popupMenu.getMenu().add("B-");
+        popupMenu.getMenu().add("AB+");
+        popupMenu.getMenu().add("Ab-");
+
+
+
+        // Set an item click listener for the PopupMenu
+        popupMenu.setOnMenuItemClickListener(item -> {
+            // Handle item selection here
+            S_Blood_group = item.getTitle().toString();
+            if (S_Blood_group.equals("None"))
+            {
+                E_B_G.setText("");
+                S_Blood_group=null;
+            }
+            else {
+                E_B_G.setText(S_Blood_group);
+            }
+            return true;
+        });
+        popupMenu.show();
+    }
+
+    //For Age
+    private void showPopupMenuForAge() {
+        PopupMenu popupMenu = new PopupMenu(this, Age);
+        popupMenu.getMenu().add("None");
+        popupMenu.getMenu().add("18-24");
+        popupMenu.getMenu().add("25-34");
+        popupMenu.getMenu().add("35-44");
+        popupMenu.getMenu().add("45-54");
+        popupMenu.getMenu().add("55 Above");
+
+
+        // Set an item click listener for the PopupMenu
+        popupMenu.setOnMenuItemClickListener(item -> {
+            // Handle item selection here
+            S_Age = item.getTitle().toString();
+            if (S_Age.equals("None"))
+            {
+                E_Age.setText("");
+                S_Age=null;
+            }
+            else {
+                E_Age.setText(S_Age);
+            }
+            return true;
+        });
+        popupMenu.show();
+    }
+    //For Gender
+    private void showPopupMenuForGender() {
+        PopupMenu popupMenu = new PopupMenu(this, Age);
+        popupMenu.getMenu().add("None");
+        popupMenu.getMenu().add("Male");
+        popupMenu.getMenu().add("Female");
+
+
+        // Set an item click listener for the PopupMenu
+        popupMenu.setOnMenuItemClickListener(item -> {
+            // Handle item selection here
+            S_Gender = item.getTitle().toString();
+            if (S_Gender.equals("None"))
+            {
+                E_Gender.setText("");
+                S_Gender=null;
+            }
+            else {
+                E_Gender.setText(S_Gender);
+            }
+            return true;
+        });
+        popupMenu.show();
+    }
+
+
+}
--- app/src/main/java/com/example/blooddonation/home_page.java
+++ app/src/main/java/com/example/blooddonation/home_page.java
@@ -1,15 +1,25 @@
 package com.example.blooddonation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class home_page extends AppCompatActivity {
 
+    TextInputLayout Search;
+    TextInputEditText E_search;
     Button button,button2;
+    ImageView Filter,Menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,11 @@ public class home_page extends AppCompatActivity {
 
         button=findViewById(R.id.button);
         button2=findViewById(R.id.button2);
+        Filter=findViewById(R.id.Filter);
+        Menu=findViewById(R.id.Menu);
+        Search=findViewById(R.id.Search);
+        E_search=findViewById(R.id.E_search);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("UseCompatLoadingForDrawables")
@@ -35,14 +50,60 @@ public class home_page extends AppCompatActivity {
                 button2.setBackground(getDrawable(R.drawable.cus_join22));
             }
         });
+        Filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(home_page.this, Filter_page.class);
+                startActivityForResult(intent,1);
+            }
+        });
+        Menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create the floating window
+                //Menu.isFocused()
+                showPopupMenuForGender();
+            }
+        });
 
 
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==1)
+        {
+            if (resultCode==RESULT_OK)
+            {
+                String Age=data.getStringExtra("Age");
+                String Blood_G=data.getStringExtra("Blood_group");
+                String Gender=data.getStringExtra("Gender");
+                E_search.setText(Gender);
+            }
+        }
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         finishAffinity();
+    }
+
+    private void showPopupMenuForGender() {
+        PopupMenu popupMenu = new PopupMenu(this, Menu);
+        popupMenu.getMenu().add("Profile");
+        popupMenu.getMenu().add("Personalisation");
+        popupMenu.getMenu().add("Security");
+        popupMenu.getMenu().add("About");
+
+
+        // Set an item click listener for the PopupMenu
+        popupMenu.setOnMenuItemClickListener(item -> {
+            // Handle item selection here
+            return true;
+        });
+        popupMenu.show();
     }
 }
