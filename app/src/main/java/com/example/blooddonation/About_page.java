ca96869c1a317e025ac0ac87a97c7ae6fa8cb291
Pranavanesh-V <pranavanesh.vimalraj@gmail.com>
2023-10-10 23:28:16 +0530

    registration page

--- app/src/main/java/com/example/blooddonation/About_page.java
+++ app/src/main/java/com/example/blooddonation/About_page.java
@@ -1,14 +1,26 @@
 package com.example.blooddonation;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class About_page extends AppCompatActivity {
+
+    Button back4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_page);
+
+        back4=findViewById(R.id.back4);
+        back4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
