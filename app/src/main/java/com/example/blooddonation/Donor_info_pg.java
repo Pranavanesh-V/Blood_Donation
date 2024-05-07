49c54c3a36e813832bbccca5061995fbe9c9d51a
prana <pranavanesh.vimalraj@gmail.com>
2024-05-07 17:45:39 +0530

    Guess the final changes made

--- app/src/main/java/com/example/blooddonation/Donor_info_pg.java
+++ app/src/main/java/com/example/blooddonation/Donor_info_pg.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,10 +22,11 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Donor_info_pg extends AppCompatActivity {
 
-    TextView T_Name, T_Phone, T_Blood, T_Mail_id, T_Address;
+    EditText T_Name, T_Phone, T_Blood, T_Mail_id, T_Address;
+    TextInputLayout Name,Phone,Blood,Mail_id,Address;
     DatabaseReference databaseReference;
     Button back;
-    String Phone,Email_id,Address,S_Location;
+    String S_Phone,Email_id, S_Address,S_Location;
     ImageView p_img;
 
     //Class is used only for displaying the details of the donars
@@ -32,11 +35,17 @@ public class Donor_info_pg extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donar_info_pg);
 
-        T_Name = findViewById(R.id.Name);
-        T_Phone = findViewById(R.id.Phone_number);
-        T_Blood = findViewById(R.id.Blood_Group);
-        T_Mail_id =findViewById(R.id.Mail_id);
-        T_Address =findViewById(R.id.Address);
+        Name=findViewById(R.id.Name);
+        Phone=findViewById(R.id.Phone_number);
+        Blood=findViewById(R.id.Blood);
+        Mail_id=findViewById(R.id.Mail_id);
+        Address=findViewById(R.id.Address);
+
+        T_Name = Name.getEditText();
+        T_Phone = Phone.getEditText();
+        T_Blood = Blood.getEditText();
+        T_Mail_id =Mail_id.getEditText();
+        T_Address =Address.getEditText();
         back=findViewById(R.id.back);
         p_img=findViewById(R.id.p_img);
 
@@ -72,17 +81,14 @@ public class Donor_info_pg extends AppCompatActivity {
                     if (S_name.equals(name)) {
                         //Fetch the data from the database
                         Email_id = snapshot.child("Email").getValue(String.class);
-                        Address = snapshot.child("Address").getValue(String.class);
-                        Phone = snapshot.child("Phone No").getValue(String.class);
+                        S_Address = snapshot.child("Address").getValue(String.class);
+                        S_Phone = snapshot.child("Phone No").getValue(String.class);
                         S_Location=snapshot.child("City").getValue(String.class);
 
                         //For displaying the email dynamically according to the size
-                        if (!(Email_id.length()<23)) {
-                            T_Mail_id.setText(Email_id.substring(0,23)+"\n"+ Email_id.substring(23));
-                        }else {
-                        T_Mail_id.setText(Email_id);}
-                        T_Address.setText(String.format("%s,\n%s", Address, S_Location));
-                        T_Phone.setText(Phone);
+                        T_Mail_id.setText(Email_id);
+                        T_Address.setText(String.format("%s,\n%s", S_Address, S_Location));
+                        T_Phone.setText(S_Phone);
                         //if image doesn't exists place a common user image for each gender
                         break;
                     }
@@ -104,7 +110,7 @@ public class Donor_info_pg extends AppCompatActivity {
 
         //Navigates to the Dial screen for the displayed Mobile Number
         T_Phone.setOnClickListener(view -> {
-            Intent intent12 =new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+Phone));
+            Intent intent12 =new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+ S_Phone));
             startActivity(intent12);
         });
     }
