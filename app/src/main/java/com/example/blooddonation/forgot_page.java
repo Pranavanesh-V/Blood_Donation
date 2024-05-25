24f34d981137ac48cd6220f848f78458584dff96
prana <pranavanesh.vimalraj@gmail.com>
2024-05-25 21:42:55 +0530

    Guess the final changes made

--- app/src/main/java/com/example/blooddonation/forgot_page.java
+++ app/src/main/java/com/example/blooddonation/forgot_page.java
@@ -3,7 +3,9 @@ package com.example.blooddonation;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.telephony.SmsManager;
 import android.text.Editable;
 import android.text.InputFilter;
@@ -35,6 +37,7 @@ public class forgot_page extends AppCompatActivity {
     Button submit;
     boolean flag;
     private static String MESSAGE;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +54,7 @@ public class forgot_page extends AppCompatActivity {
         MESSAGE = otp_generator.generate();
         //get the username from the previous page for later use
         Intent intent=getIntent();
-        String username=intent.getStringExtra("Username");
+        username=intent.getStringExtra("Username");
 
         //Edittext to get the string value of the number of the user
         EditText E_phone=phone.getEditText();
@@ -87,15 +90,6 @@ public class forgot_page extends AppCompatActivity {
         };
         E_phone.addTextChangedListener(login);
 
-        flag=false;
-        // Check SMS permission
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
-            // Permission already granted, send SMS
-            flag=true;
-        } else {
-            // Request SMS permission
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, SMS_PERMISSION_REQUEST_CODE);
-        }
 
         //Button check if the number is of the respective user
         //And sends the otp to that number and navigates to the next activity
@@ -113,20 +107,7 @@ public class forgot_page extends AppCompatActivity {
                     //if same send the otp
                     if (num.equals(S_phone))
                     {
-                        //send otp in sms
-
-                        if (flag) {
-                            sendSms();
-                            Intent intent1 = new Intent(forgot_page.this, otp_auth.class);
-                            intent1.putExtra("number", S_phone);
-                            intent1.putExtra("OTP", MESSAGE);
-                            intent1.putExtra("U_name", username);
-                            startActivity(intent1);
-                        }
-                        else
-                        {
-                            Toast.makeText(forgot_page.this, "Provide sms permission", Toast.LENGTH_SHORT).show();
-                        }
+                        checkSmsPermission();
                     }
                     else
                     {
@@ -147,8 +128,29 @@ public class forgot_page extends AppCompatActivity {
 
                 }
             });
-
         });
+    }
+
+    // Method to check and request SMS permission
+    private void checkSmsPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            // If permission is not granted, request it
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
+                // Show rationale and request permission
+                Toast.makeText(this, "SMS permission is required to send messages.", Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, SMS_PERMISSION_REQUEST_CODE);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, SMS_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            // If permission is already granted, send SMS
+            sendSms();
+            Intent intent1 = new Intent(forgot_page.this, otp_auth.class);
+            intent1.putExtra("number", S_phone);
+            intent1.putExtra("OTP", MESSAGE);
+            intent1.putExtra("U_name", username);
+            startActivity(intent1);
+        }
     }
 
     @Override
@@ -157,9 +159,26 @@ public class forgot_page extends AppCompatActivity {
         if (requestCode == SMS_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission granted, send SMS
+                sendSms();
+                Intent intent1 = new Intent(forgot_page.this, otp_auth.class);
+                intent1.putExtra("number", S_phone);
+                intent1.putExtra("OTP", MESSAGE);
+                intent1.putExtra("U_name", username);
+                startActivity(intent1);
             } else {
-                // Permission denied, show a message or handle the situation
-                Toast.makeText(this, "SMS permission denied", Toast.LENGTH_SHORT).show();
+                // Permission denied
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
+                    // User checked "Don't ask again"
+                    Toast.makeText(this, "Permission denied. Please enable SMS permission in app settings.", Toast.LENGTH_LONG).show();
+                    // Open app settings
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, "Permission denied. Cannot send SMS.", Toast.LENGTH_SHORT).show();
+                }
             }
         }
     }
